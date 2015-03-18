0bb52203b00febd317c7c25ef99d9450c2412ff7
mingfei <mingfei.net@gmail.com>
2015-03-18 16:46:50 +0800

    edit SessionFactoryUtil
    add a new method: getSqlSessionFactoryVisXML;

--- src/main/java/demo/util/SessionFactoryUtil.java
+++ src/main/java/demo/util/SessionFactoryUtil.java
@@ -1,7 +1,8 @@
 package demo.util;
 
-import demo.mapper.UserMapper;
+import demo.data.UserMapper;
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +11,8 @@ import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author mingfei.net@Gmail.com
@@ -22,6 +25,10 @@ public class SessionFactoryUtil {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
 
+    /**
+     * get SqlSessionFactory instance via MyBatis Java classes.
+     *
+     */
     public static SqlSessionFactory getSqlSessionFactory() {
         DataSource dataSource = new PooledDataSource(DRIVER, URL, USERNAME, PASSWORD);
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
@@ -29,5 +36,35 @@ public class SessionFactoryUtil {
         Configuration configuration = new Configuration(environment);
         configuration.addMapper(UserMapper.class);
         return new SqlSessionFactoryBuilder().build(configuration);
+    }
+
+    /**
+     * get SqlSessionFactory instance via XML configurations.
+     *
+     */
+
+    public static SqlSessionFactory getSqlSessionFactoryViaXML() {
+        String resource = "mybatis-config.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            return new SqlSessionFactoryBuilder().build(inputStream);
+
+            /**
+             *    If not set properties in mybatis-config.xml,
+             *    use build(inputStream, properties) method.
+             */
+
+//            Properties properties = new Properties();
+//            properties.load(new FileInputStream("src/main/resources/jdbc.properties"));
+//            return new SqlSessionFactoryBuilder().build(inputStream, properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSqlSessionFactory());
+        System.out.println(getSqlSessionFactoryViaXML());
     }
 }
