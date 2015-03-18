d91a43fb41a8ef4aecc8bc6a24c83a2ea6e9d57c
mingfei <mingfei.net@gmail.com>
2015-03-18 17:38:24 +0800

    edit SelectTest class
    add ID constant;

--- src/main/java/demo/test/SelectTest.java
+++ src/main/java/demo/test/SelectTest.java
@@ -17,6 +17,8 @@ public class SelectTest {
     private static SqlSessionFactory sqlSessionFactory;
     private static SqlSession sqlSession;
 
+    private static final int ID = 4;
+
     static {
 //        sqlSessionFactory = SessionFactoryUtil.getSqlSessionFactory();
         sqlSessionFactory = SessionFactoryUtil.getSqlSessionFactoryViaXML();
@@ -27,19 +29,36 @@ public class SelectTest {
         sqlSession.close();
     }
 
+    /**
+     * use UserMapper class configuration.
+     * @return
+     */
     private static User selectById() {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         return userMapper.getUserById(1);
     }
 
+    /**
+     * use MyBatis native method selectOne.
+     * @return
+     */
     private static User selectOne() {
-        return sqlSession.selectOne("demo.data.UserMapper.getUserById", 1);
+        return sqlSession.selectOne("demo.data.UserMapper.getUserById", ID);
+    }
+
+    /**
+     * user userMapper.xml configuration.
+     * @return
+     */
+    private static User selectUserViaXML() {
+        return sqlSession.selectOne("demo.data.UserMapper.selectUserViaXML", ID);
     }
 
     public static void main(String[] args) {
         try {
             System.out.println("selectById: " + selectById());
             System.out.println("selectOne: " + selectOne());
+            System.out.println("selectUserViaXML: " + selectUserViaXML());
         } finally {
             close();
         }
